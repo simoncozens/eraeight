677d495a7fb79160b0a75229a0a0e179c94f5d92
Simon Cozens <simon@simon-cozens.org>
2009-11-27 12:35:34 +0000

    Remove dependency on HTTP::Engine, go direct to Plack; should help Win32 compatibility.

--- lib/EraEight.pm
+++ lib/EraEight.pm
@@ -4,6 +4,7 @@ our %args;
 our $start;
 our $VERSION = "1.00";
 use Net::Amazon;
+use Plack::Response;
 our $ua_us; 
 our $ua_uk; 
 my $hires_loaded = 0;
@@ -12,8 +13,6 @@ use Data::Page;
 use EraEight::DBI;
 use List::Util qw/max/;
 use Template;
-use HTTP::Engine;
-use HTTP::Engine::Middleware;
 use strict;
 use warnings;
 use Class::DBI::Loader;
@@ -25,18 +24,16 @@ my $query_parser = KinoSearch::QueryParser::QueryParser->new(
     analyzer => $analyzer, fields   => [ 'title', 'author', 'editor' ],
     default_boolop => 'AND',
 );
+use Plack::Request;
+use Plack::App::File;
 
-sub import {
+sub app { 
     my $self = shift;
     %args = ( # Defaults go first
         dsn => "dbi:SQLite:heritage.db",
-        interface => "ServerSimple",
-        template_path => ["user_templates", "templates"],
-        port => "4848",
-        host => "0.0.0.0",
+        template_path => [ "user_templates", "templates"],
         @_
     );
-    my $mw = HTTP::Engine::Middleware->new( { method_class => 'HTTP::Engine::Request' });
     if (!$args{amazon_key_id}) { die "You need to include your Amazon key" };
     if (!$args{amazon_secret_key}) { die "You need to include your Amazon secret" };
     $ua_us = Net::Amazon->new(token => $args{amazon_key_id}, secret_key => $args{amazon_secret_key}); 
@@ -44,13 +41,6 @@ sub import {
 
     use EraEight::DBI;
     EraEight::DBI->load($args{dsn});
-    for (@{ref($args{template_path}) ? $args{template_path} : [$args{template_path}]}) {
-        $mw->install( 'HTTP::Engine::Middleware::Static' => {
-            regexp  => qr{^/static/(.+)$},
-            docroot => $_,
-            is_404_handler => 0,
-        });
-    }
     my $t = Template->new({
         INCLUDE_PATH => $args{template_path},
         PRE_PROCESS  => "header",
@@ -58,30 +48,41 @@ sub import {
         COMPILE_DIR => $args{compiled_templates},
         COMPILE_EXT => ".ttc"
     });
-    HTTP::Engine->new(
-        interface => {
-            module => $args{interface},
-            args   => { %args },
-            request_handler => $mw->handler(sub {$_[0]->{template}=$t;handle_request(@_)}),
-        },
-    )->run;
+    sub { 
+        my $env = shift;
+        my $req = Plack::Request->new($env);
+        # Fake static middleware because it won't let us have multiple
+        # paths
+        if ($req->path =~ /^\/static/) {
+            for (@{ref($args{template_path}) ? $args{template_path} : [$args{template_path}]}) {
+                next unless -f "$_/".$req->path;
+                my $file = Plack::App::File->new({ root => $_ });
+                return $file->call({ %$env, PATH_INFO => $env->{PATH_INFO} });
+            }
+            return [404, ['Content-Type' => 'text/plain'], ['not found']];
+        }
+        my $m = $self->new(%args);
+        $m->{template_engine} = $t;
+        $m->handle_request($req)->finalize;
+    }
 }
 
 sub new { bless {}, shift } # For templates
 
 sub handle_request {
-    my $req = shift;
+    my ($self, $req) = @_;
     $start = $hires_loaded ? [gettimeofday()] : 0;
     my $page;
-    my $res = HTTP::Engine::Response->new;
+    my $res = Plack::Response->new;
     my (undef, $action, @args) = split /\//,  $req->path;
-    if (defined $action and $action eq "details") { return EraEight->details($req) }
+    if (defined $action and $action eq "details") { return $self->details($req) }
     my $q = $req->parameters()->{q};
     if ($q) { 
         my ($pager, @books) = search($q, $req->parameters()->{page}, $req->parameters()->{epp});
-        return EraEight->respond($req, "", "results", books => \@books, pager => $pager);
+        if ($req->parameters()->{btnI}) { return $self->details($req, $books[0]) }
+        return $self->respond($req, "", "results", books => \@books, pager => $pager);
     }
-    return EraEight->respond($req, "", "searchbox");
+    return $self->respond($req, "", "searchbox");
 }
 
 sub _try_amazon {
@@ -97,28 +98,29 @@ sub _try_amazon {
 }
 
 sub details {
-    my ($self, $req) = @_;
-    my ($book) = EraEight::Books->search(book => $req->parameters()->{book});
-    if (!$book) { return EraEight->respond($req, "", "searchbox"); }
+    my ($self, $req, $book) = @_;
+    $book ||= EraEight::Books->search(book => $req->parameters()->{book});
+    if (!$book) { return $self->respond($req, "", "searchbox"); }
     if ($book->book =~ /^[0-9X]{10}$/ and !$book->amazon and fork) {
         _try_amazon($book->book, $ua_us) ||
         _try_amazon($book->book, $ua_uk);
         exit;
     }
-    return EraEight->respond($req, "", "details", book => $book);
+    return $self->respond($req, "", "details", book => $book);
 }
 
 sub respond {
     my ($self, $req, $action, $template, @args) = @_;
     my $out;
-    my $res = HTTP::Engine::Response->new();
-    $req->{template}->process($template, { 
+    my $res = Plack::Response->new();
+    $res->status(200);
+    $self->{template_engine}->process($template, { 
         @args,
         action => $action, 
         req => $req, 
         args => \%args,
         e8 => EraEight->new(),
-        }, \$out) ? $res->body($out) : $res->body($req->{template}->error);
+        }, \$out) ? $res->body($out) : $res->body($self->{template_engine}->error);
     return $res;
 }
 
